345bd030d81f60087d303f52e5aaeb6472e4c61c
Amir Mardanov <mardanov@tochka.com>
2021-07-03 21:36:48 +0300

    Initial commit

---
+++ SummerPracticeiOS2021/Main/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  SummerPracticeiOS2021
+//
+//  Created by Марданов Амир on 03.07.2021.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBar.tintColor = .black
+    }
+}
