0d473b9786ba3aa8af1a34b5eac94bf70d8b9b55
AndrewOch <38253789+AndrewOch@users.noreply.github.com>
2021-07-05 18:53:47 +0300

    added user, checklistCell structures and empty database

---
+++ SummerPracticeiOS2021/Model/Database.swift
@@ -0,0 +1,16 @@
+//
+//  Database.swift
+//  SummerPracticeiOS2021
+//
+//  Created by itisioslab on 05.07.2021.
+//
+
+import UIKit
+
+struct Database {
+
+    var users:[User] = []
+    
+    var films:[Film] = []
+    
+}
---
+++ SummerPracticeiOS2021/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  SummerPracticeiOS2021
+//
+//  Created by itisioslab on 05.07.2021.
+//
+
+import UIKit
+
+struct User {
+    
+    var name: String
+    var image: UIImage
+    var favoriteGenre: Genre
+    var password: String
+    var friends: [User]
+    var checklist: [ChecklistCell]
+    
+}
