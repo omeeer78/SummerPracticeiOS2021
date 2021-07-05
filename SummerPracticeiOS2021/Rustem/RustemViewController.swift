f21edb48d0c044d7cbfe8ad00bd98bb638665620
rustemd02 <rustemd02@mail.ru>
2021-07-05 17:44:01 +0300

    Added basic profile interface

--- SummerPracticeiOS2021/Rustem/RustemViewController.swift
+++ SummerPracticeiOS2021/Rustem/RustemViewController.swift
@@ -9,21 +9,33 @@ import UIKit
 
 class RustemViewController: UIViewController {
 
+    @IBOutlet weak var nickname: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var wantToWatch: UIButton!
+    @IBOutlet weak var alreadyWatched: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func editProfile(_ sender: UIBarButtonItem) {
+        guard let editVC = storyboard?.instantiateViewController(withIdentifier: "EditViewController") else { return  }
+        navigationController?.pushViewController(editVC, animated: true)
     }
-    */
+    
+    @IBAction func goToWatchList(_ sender: UIButton) {
+        guard let watchList = storyboard?.instantiateViewController(withIdentifier: "EditViewController") else { return }
+        navigationController?.pushViewController(watchList, animated: true)
+    }
+    
+    @IBAction func goToAlreadyWatched(_ sender: UIButton) {
+    }
+    
+    
 
 }
