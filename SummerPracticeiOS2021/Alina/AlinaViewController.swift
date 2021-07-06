f48e1a4d437315d3ca2a64b5d885824ced370c1a
Alina <zikibikk@gmail.com>
2021-07-06 16:02:02 +0300

    added news feed

--- SummerPracticeiOS2021/Alina/AlinaViewController.swift
+++ SummerPracticeiOS2021/Alina/AlinaViewController.swift
@@ -15,15 +15,45 @@ class AlinaViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        newsUITableView.dataSource = self
+        newsUITableView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension AlinaViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        actions.count
     }
     
-    
-    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ShareNewsUITableViewCell", for: indexPath) as? ShareNewsUITableViewCell else {
+            return UITableViewCell()
+        }
+        cell.setData(film: actions[indexPath.row].recommendedFilm,
+                     friend: actions[indexPath.row].friend)
+        return cell
+    }
 }
+
+extension AlinaViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        405
+    }
+    
+//заменить AlinaViewController на экран фильма, это модальный переход на него
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        guard let alinaViewController = storyboard?.instantiateViewController(withIdentifier: "AlinaViewController") as? AlinaViewController else { return }
+//    alinaViewController.film = фсешщты[indexPath.row].film
+//    present(alinaViewController, animated: true)
+//    }
+}
+
+
 
 struct Action {
     var user: User
     var friend: User
+    var recommendedFilm: Film
 }
